4c7a4aec762efa69416496f11b28c79a35112eda
Example Maintainer <maintainer@example.com>
2026-06-18 13:23:38 +0900

    Fix example wiki icon rendering

--- example-wiki/docs/.vitepress/theme/index.ts
+++ example-wiki/docs/.vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 import FooterLinks from './components/FooterLinks.vue'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import './custom.css'
 
 export default {
